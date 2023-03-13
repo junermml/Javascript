a1653e328b67d60329fb2c2297fb2bf6e1b567f5
junereloj <124117247+junermml@users.noreply.github.com>
2023-03-13 11:21:19 +0800

    Add files via upload

---
+++ Javascript/JS/function.js
@@ -0,0 +1,132 @@
+/*//oncontextmenu
+function first(){
+
+    alert ("you have right clicked a button");
+}
+
+function fist(){
+
+    alert("You have right clicked a link");
+}
+
+
+//onmouseout 
+function second(){
+
+    alert ("you have moved out of the button");
+}
+
+function seco(){
+
+    alert("You have moved out of the link");
+}
+
+
+//onkeydown 
+function third(){
+
+    alert ("you have pressed a key inside text input!");
+}
+
+function thi(){
+
+    alert("you have pressed a key inside textarea!");
+}
+
+
+//onkeyup 
+function four(){
+
+    alert ("you have released a key inside text input");
+}
+
+function fo(){
+
+    alert("you have released a key inside textarea!");
+}
+
+
+//onkeypress 
+function five(){
+
+    alert ("you have pressed a key inside text input");
+}
+
+function fiv(){
+
+    alert("you have pressed a key inside textarea");
+}*/
+
+/*onfocus 
+function five(){
+
+    alert ("you have pressed a key inside text input");
+}
+
+function fiv(){
+
+    alert("you have pressed a key inside textarea");
+}*/
+
+
+/*onblur
+function five(){
+
+    alert ("you have pressed a key inside text input");
+}
+
+function fiv(){
+
+    alert("you have pressed a key inside textarea");
+}*/
+
+//ONSUBMIT
+//function it(){
+
+    //alert("Form data will be submmitedn to the server!")
+     
+//}
+
+//ONLOAD
+/*function it(){
+
+    alert("Page is loaded successfully!")
+     
+}*/
+
+//ONRESIZE
+/*function myFunction() {
+    var w = window.outerWidth;
+    var h = window.outerHeight;
+    var txt = "Window size: width=" + w + ", height=" + h;
+    document.getElementById("result").innerHTML = txt;
+  }*/
+
+
+/*var now = new Date();
+
+var dayOfWeek = now.getDay();
+
+if (dayOfWeek=3)
+{
+    alert("Have a nice wednesday");
+}*/
+
+/*var now = new date ();
+
+var dayOfWeek = now.getDay();
+
+if(dayOfWeek == 1) {
+    alert("Have a nice monday!");
+}
+
+else if(dayOfWeek == 5) {
+    alert("Have a nice friday");
+}
+
+else 
+{
+    alert("Have a nice day!");
+}*/
+
+
